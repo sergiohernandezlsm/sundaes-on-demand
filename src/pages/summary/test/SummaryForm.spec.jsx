5058b6b1d94dbbbff25cdb9dd8c60899bcbcdbe3
Sergio <sergio.hernandez@littlestarmedia.com>
2021-12-21 15:33:52 +0000

    popover test

--- src/pages/summary/test/SummaryForm.spec.jsx
+++ src/pages/summary/test/SummaryForm.spec.jsx
@@ -1,5 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 describe("<SummaryForm />", () => {
   test("testing checkbox", () => {
@@ -12,14 +17,41 @@ describe("<SummaryForm />", () => {
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     expect(checkbox).toBeChecked();
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
+  });
+
+  test("pop over test", async () => {
+    render(<SummaryForm />);
+
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+
+    // popover not in the page
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears is hover on checkbox label
+    const termsAndConditions = screen.getByText(/terms and Conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse over
+    userEvent.unhover(termsAndConditions);
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
   });
 });
